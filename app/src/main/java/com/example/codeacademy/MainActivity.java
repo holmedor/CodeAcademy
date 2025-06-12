c7e5a01056660f8848a0a1a1943e92a5a36a120a
holmedor <holmedor@gmail.com>
2025-06-12 21:51:03 +0200

    12/06/2025: MainActivity con intents a "QuienesSomos", "Cursos" y "Contacto"

--- app/src/main/java/com/example/codeacademy/MainActivity.java
+++ app/src/main/java/com/example/codeacademy/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.codeacademy;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,9 @@ import androidx.core.view.WindowInsetsCompat;
 //public class MainActivity extends AppCompatActivity {
 public class MainActivity extends Activity {
 
+    private ImageView imgvQuienesSomos;
+    private ImageView imgvCursos;
+    private ImageView imgvContacto;
     Button boton;
 
     @Override
@@ -22,6 +27,27 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        imgvQuienesSomos=(ImageView) findViewById(R.id.imgvQuienesSomos);
+        imgvCursos=(ImageView) findViewById(R.id.imgvCursos);
+        imgvContacto=(ImageView) findViewById(R.id.imgvContacto);
+        imgvQuienesSomos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, QuienesSomosActivity.class));
+            }
+        });
+        imgvCursos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, CursosActivity.class));
+            }
+        });
+        imgvContacto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, ContactoActivity.class));
+            }
+        });
 //        boton = (Button) findViewById(R.id.btnLlamar);
 //        boton.setOnClickListener(new View.OnClickListener(){
 /*            @Override
